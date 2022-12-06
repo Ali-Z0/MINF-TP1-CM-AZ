163d8a32aab799211fa3e9b19634bb5b21a06f0c
Ali-Z0 <ali.zoubir@bluewin.ch>
2022-12-06 15:21:14 +0100

    Filtre + Consignes

--- firmware/src/GestPWM.c
+++ firmware/src/GestPWM.c
@@ -33,14 +33,26 @@ void GPWM_Initialize(S_pwmSettings *pData)
 // Obtention vitesse et angle (mise a jour des 4 champs de la structure)
 void GPWM_GetSettings(S_pwmSettings *pData)	
 {
-     
-    // Lecture du convertisseur AD
-    if(pData->cntAdc < 1)
-    {
-        
-    }
+    /* Gestion de la lecture d'ADC avec filtrage */
+    GPWM_ReadAdcFiltered(pData);
+    
+    /* Calcul de la consigne de vitesse */
+    pData->absSpeed = pData->adcResFilt_Can0 * SPEED_RATIO;
+    pData->SpeedSetting = (float)pData->absSpeed - (float)99;
+    pData->absSpeed = abs(pData->SpeedSetting);
+    
+    
+    /* Calcul consigne de la vitesse */
+    pData->absAngle = pData->adcResFilt_Can1 * (180 / ADC_RES);
+    pData->AngleSetting = (float)pData->absSpeed - (float)90;
+    pData->absAngle = abs(pData->AngleSetting);
+    
+    
+    
+    
+    
 
-    pData->ADCres[0] = BSP_ReadAllADC();
+    
     // conversion
     
 }
@@ -64,4 +76,38 @@ void GPWM_ExecPWMSoft(S_pwmSettings *pData)
     
 }
 
-
+uint8_t GPWM_ReadAdcFiltered(S_pwmSettings *pData)
+{
+    uint8_t i = 0; 
+    uint8_t flagZero = 0; 
+    
+    /* Si le compteur de nombre d'échantillons pour filtrage atteint */
+    if(pData->cntAdc < FILTER_SIZE)
+    {
+        /* Sauvegarde les cannaux dans le buffer de mesure */
+        pData->AdcResBuff[pData->cntAdc] = BSP_ReadAllADC();
+        /* Incremente le timer de filtrage */
+        pData->cntAdc = pData->cntAdc +1;
+    }
+    else
+    {
+        /* Relance le filtrage */
+        pData->cntAdc = 0;
+    }
+    /* Somme du buffer pour le filtrage */
+    for(i = 0; i<FILTER_SIZE; i++)
+    {
+        pData->adcResFilt_Can0 = pData->adcResFilt_Can0 + pData->AdcResBuff[i].Chan0;
+        pData->adcResFilt_Can1 = pData->adcResFilt_Can1 + pData->AdcResBuff[i].Chan1;
+        
+        if((pData->AdcResBuff[i].Chan0 == 0) || (pData->AdcResBuff[i].Chan1 == 0))
+            flagZero = 1;
+    }  
+    /* Moyenne du buffer avec protection de division avec 0 */
+    if((pData->adcResFilt_Can0 != 0) && (pData->adcResFilt_Can1 != 0))
+    {
+        pData->adcResFilt_Can0 /= FILTER_SIZE;
+        pData->adcResFilt_Can1 /= FILTER_SIZE;
+    }
+    return flagZero;
+}
--- firmware/src/GestPWM.h
+++ firmware/src/GestPWM.h
@@ -20,6 +20,11 @@
 /*--------------------------------------------------------*/
 // Définition des fonctions prototypes
 /*--------------------------------------------------------*/
+#define FILTER_SIZE 10
+#define ADC_RES 1023
+#define ABS_MAX_SPEED 198
+#define SPEED_RATIO (ABS_MAX_SPEED / ADC_RES)
+
 
 
 typedef struct {
@@ -28,7 +33,9 @@ typedef struct {
     int8_t SpeedSetting; // consigne vitesse -99 à +99
     int8_t AngleSetting; // consigne angle  -90 à +90
     
-    S_ADCResults AdcResBuff[10]; // Données ADC
+    S_ADCResults AdcResBuff[FILTER_SIZE]; // Données ADC
+    float adcResFilt_Can0;
+    float adcResFilt_Can1;
     uint8_t cntAdc;
 } S_pwmSettings;
 
@@ -37,6 +44,7 @@ extern S_pwmSettings PWMData;
 void GPWM_Initialize(S_pwmSettings *pData);
 
 // Ces 3 fonctions ont pour paramètre un pointeur sur la structure S_pwmSettings.
+uint8_t GPWM_ReadAdcFiltered(S_pwmSettings *pData);
 void GPWM_GetSettings(S_pwmSettings *pData);	// Obtention vitesse et angle
 void GPWM_DispSettings(S_pwmSettings *pData);	// Affichage
 void GPWM_ExecPWM(S_pwmSettings *pData);		// Execution PWM et gestion moteur.
